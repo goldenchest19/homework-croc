39b72e2b164ad56841c421ef1409631a4ee2c269
goldenchest19 <kirya-ignatev-2016@mail.ru>
2022-10-14 15:25:58 +0300

    added keyboard input in Task2 and improve method printBytes

--- src/ru/croc/task2/Task2.java
+++ src/ru/croc/task2/Task2.java
@@ -18,21 +18,22 @@ public class Task2 {
         BigDecimal numberZero = new BigDecimal(0);
         if (number.compareTo(numberZero) == -1) {
             System.out.println("Введены некоректные данные");
-        } else {
-            int count = 0;
-            String[] degreeByte = new String[]{"B", "KB", "MB", "GB", "TB", "PB"};
-            BigDecimal newNumber = number;
-            BigDecimal checkingNumber = new BigDecimal(1024);
-
-            while (newNumber.compareTo(checkingNumber) == 1 || newNumber.compareTo(checkingNumber) == 0) {
-                count++;
-                newNumber = newNumber.divide(checkingNumber);
-            }
-
-            double someValue = newNumber.doubleValue();
-            String finalNumber = String.format("%.1f", someValue);
-            System.out.println(Double.parseDouble(finalNumber) + " " + degreeByte[count]);
+            return;
         }
+        int count = 0;
+        String[] degreeByte = new String[]{"B", "KB", "MB", "GB", "TB", "PB"};
+        BigDecimal newNumber = number;
+        BigDecimal checkingNumber = new BigDecimal(1024);
+
+        while (newNumber.compareTo(checkingNumber) == 1 || newNumber.compareTo(checkingNumber) == 0) {
+            count++;
+            newNumber = newNumber.divide(checkingNumber);
+        }
+
+        double someValue = newNumber.doubleValue();
+        String finalNumber = String.format("%.1f", someValue);
+        System.out.println(Double.parseDouble(finalNumber) + " " + degreeByte[count]);
+
     }
 }
 
