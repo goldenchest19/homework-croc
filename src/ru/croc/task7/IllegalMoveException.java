bb394e1fa53e8795949af748f5221c16f4018948
goldenchest19 <kirya-ignatev-2016@mail.ru>
2022-11-05 01:54:03 +0300

    update IllegalMoveException

--- src/ru/croc/task7/IllegalMoveException.java
+++ src/ru/croc/task7/IllegalMoveException.java
@@ -1,8 +1,18 @@
 package ru.croc.task7;
 
 public class IllegalMoveException extends Exception {
+    ChessPosition prevChessPosition;
+    ChessPosition currentChessPosition;
+
     public IllegalMoveException(ChessPosition prevChessPosition, ChessPosition currentChessPosition) {
-        System.out.println("конь так не ходит " + prevChessPosition.toString() + " -> " +
-                currentChessPosition.toString());
+        this.prevChessPosition = prevChessPosition;
+        this.currentChessPosition = currentChessPosition;
+        System.out.println(getMessage());
+    }
+
+    @Override
+    public String getMessage() {
+        return "конь так не ходит " + prevChessPosition.toString() + " -> " +
+                currentChessPosition.toString();
     }
 }
