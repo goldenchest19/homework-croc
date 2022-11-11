2458e883b28817eba818368fd26ee38d30c0f5e8
goldenchest19 <kirya-ignatev-2016@mail.ru>
2022-11-11 12:19:28 +0300

    add task8

--- src/ru/croc/task8/Task8.java
+++ src/ru/croc/task8/Task8.java
@@ -1,38 +1,26 @@
 package ru.croc.task8;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Task8 {
 
     public static void main(String[] args) throws IOException {
         String path1 = args[0];
-        String line = "src/ru/croc/task8/input.txt";
-        String[] arr = line.split("/");
-//        for (String s : arr) {
-//            System.out.println(s);
-//        }
-//        System.out.println(String.valueOf(arr));
-
-//        System.out.println(path1);
-        readFile();
+        readFile(path1);
     }
 
-    public static void readFile() throws IOException {
+    public static void readFile(String path) throws IOException {
         int count = 0;
-        String[] arr = new String[10];
-        Path path = Path.of("src", "ru", "croc", "task8", "input.txt");
-//        Path path2 = Path.of(String.valueOf(arr));
-        try (Scanner scanner = new Scanner()) {
+        File myObj = new File(path);
 
+        try (Scanner scanner = new Scanner(myObj)) {
             while (scanner.hasNext()) {
                 String word = scanner.next();
-                System.out.println(word);
                 count++;
             }
         }
         System.out.println(count);
     }
-
 }
