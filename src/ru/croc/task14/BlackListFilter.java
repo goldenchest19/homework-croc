3b6866b70cf8819b5328ba8c0406b4ff2faa7d64
goldenchest19 <kirya-ignatev-2016@mail.ru>
2022-11-29 23:32:50 +0300

    refactor code

--- src/ru/croc/task14/BlackListFilter.java
+++ src/ru/croc/task14/BlackListFilter.java
@@ -1,13 +1,11 @@
 package ru.croc.task14;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Predicate;
 
 public interface BlackListFilter {
-    
+
     default <T> List<T> filterComments(Iterable<T> comments, Predicate<T> blackList) {
         List<T> returnList = new ArrayList<>();
         for (T comment : comments) {
--- src/ru/croc/task14/Filter.java
+++ src/ru/croc/task14/Filter.java
@@ -1,7 +1,8 @@
 package ru.croc.task14;
 
-import java.util.*;
-import java.util.function.Predicate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class Filter implements BlackListFilter {
     public List<String> getCommentsFilter(List<String> comments, Set<String> blackList) {
