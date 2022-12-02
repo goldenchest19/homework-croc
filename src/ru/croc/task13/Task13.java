5c03f111c2be6d709f98634e377f4b7a7d0669ac
goldenchest19 <kirya-ignatev-2016@mail.ru>
2022-12-02 22:41:28 +0300

    add input from keyboard

--- src/ru/croc/task13/Task13.java
+++ src/ru/croc/task13/Task13.java
@@ -1,17 +1,20 @@
 package ru.croc.task13;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Task13 {
 
     public static void main(String[] args) throws IOException {
+        // input - 1,2,4
         // при тесте [3,3,3,1,1,2,5,4] , [1,1,3,4,4,4,5], [2,3,3,3,5]
 //        получаем: 1 шаг: [3,3,3,1,1,2,5,4] , [1,1,3,4,4,4,5] , [2,3,3,3,5]
 //        2 шаг: [3,3,3,5] , [3,5]
 //        3 шаг: {3:4, 5:2}
 //        Вывод - Дюна
-
-        RecommendationSystem recommendationSystem = new RecommendationSystem("1,2,4");
+        Scanner scanner = new Scanner(System.in);
+        String filmsByUser = scanner.nextLine();
+        RecommendationSystem recommendationSystem = new RecommendationSystem(filmsByUser);
         System.out.println(recommendationSystem.searchFilm());
     }
 }
